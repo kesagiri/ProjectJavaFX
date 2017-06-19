cef1118b1559f78a82553b7825639c7f3d5e9871
kesagiri <antony.rus@yandex.ru>
2017-06-19 21:12:55 +0300

    Table on the left is filled

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -4,7 +4,11 @@ package ch.makery.address;/**
 
 import java.io.IOException;
 
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -15,6 +19,29 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    // Конструктор
+    public MainApp() {
+        // В качестве образца добавляем некоторые данные
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Возвращает данные в виде наблюдаемого списка адресатов.
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -57,6 +84,11 @@ public class MainApp extends Application {
 
             // Помещаем сведения об адресатах в центр корневого макета.
             rootLayout.setCenter(personOverview);
+
+            // Даём контроллеру доступ к главному приложению.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
